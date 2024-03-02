af1f658510f3ed58ce6a3df76bd3807413c8bb14
branewaveai <157820981+branewaveai@users.noreply.github.com>
2024-03-03 03:15:36 +0530

    Add files via upload

---
+++ src/components/navigation/navigation.data.ts
@@ -0,0 +1,20 @@
+import type { Navigation } from '@/interfaces/navigation'
+
+export const navigations: Navigation[] = [
+  {
+    label: 'Home',
+    path: '#', // '/',
+  },
+  {
+    label: 'Treatments',
+    path: 'popular-course', // '/popular-course',
+  },
+  {
+    label: 'Testimonial',
+    path: 'testimonial', // '/testimonial',
+  },
+  {
+    label: 'Doctors',
+    path: 'mentors', // '/mentors',
+  },
+]
