326eeea55b0f36de16a8026aad2715656a2b7e26
branewaveai <157820981+branewaveai@users.noreply.github.com>
2024-03-10 19:48:38 +0530

    Update thanks.tsx

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -3,6 +3,14 @@ import React from 'react';
 const ThanksPage: React.FC = () => {
   return (
     <div>
+      <Script id="conversion-analytics-script" strategy="lazyOnload">
+        {`
+          gtag('event', 'conversion', {
+            'send_to': 'AW-16480308560/pEK6CJX2j5gZENCatrI9',
+            'transaction_id': ''
+          });
+        `}
+      </Script>
       <h1>Thanks</h1>
     </div>
   );
