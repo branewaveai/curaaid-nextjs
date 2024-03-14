a900059749f8a12ff6941d13c5fe043a690acd5a
branewaveai <157820981+branewaveai@users.noreply.github.com>
2024-03-15 00:34:02 +0530

    Create oncology.tsx

---
+++ src/pages/oncology.tsx
@@ -0,0 +1,37 @@
+import { MainLayout } from "@/components/layout";
+import { NextPageWithLayout } from "@/interfaces/layout";
+import dynamic from "next/dynamic";
+// import { HomeFeature, HomeHero, HomePopularCourse, HomeTestimonial, HomeOurMentors, DynamicHomeNewsLetter } from '@/components/home'
+
+const DynamicHomeHero = dynamic(() => import("../components/home/oncoHero"));
+const DynamicHomeFeature = dynamic(() => import("../components/home/feature"));
+const DynamicHomePopularCourse = dynamic(
+  () => import("../components/home/expert-doctors")
+);
+const DynamicHomeTestimonial = dynamic(
+  () => import("../components/home/testimonial")
+);
+const DynamicHomeOurMentors = dynamic(
+  () => import("../components/home/hospitals")
+);
+const DynamicHomeTreatment = dynamic(
+  () => import("../components/home/treatment")
+);
+const DynamicHomeNewsLetter = dynamic(
+  () => import("../components/home/newsletter")
+);
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <>
+      <DynamicHomeHero />
+      <DynamicHomeOurMentors />
+      <DynamicHomePopularCourse />
+      <DynamicHomeTreatment />
+    </>
+  );
+};
+
+Home.getLayout = (page) => <MainLayout>{page}</MainLayout>;
+
+export default Home;
