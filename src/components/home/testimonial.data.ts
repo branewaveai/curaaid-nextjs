ba2a2c3202db00a35e3a3905434037f7c7328709
Vijay Maurya <vijaymaurya912082@gmail.com>
2024-03-16 17:40:02 +0530

    Testimonials Added

--- src/components/home/testimonial.data.ts
+++ src/components/home/testimonial.data.ts
@@ -2,8 +2,8 @@ import type { Testimonial } from '@/interfaces/testimonial'
 
 export const data: Array<Testimonial> = [
   {
-    id: 1,
-    title: 'Detailed learning materials',
+    id: "YzPW2e3DruI",
+    title: 'Patient from Nigeria | Complex Tracheostomy treatment in India',
     content:
       'Classes that provide very detailed material in term of making UI UX Design starting team making low and hight quality, system designs, using data layout and make prototypes and testing.',
     user: {
@@ -14,8 +14,8 @@ export const data: Array<Testimonial> = [
     },
   },
   {
-    id: 2,
-    title: 'Best Quality Online Course!',
+    id: "-b9_mbNVvKI",
+    title: 'Patient from Kenya | Kidney transplant in India',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     user: {
@@ -26,8 +26,8 @@ export const data: Array<Testimonial> = [
     },
   },
   {
-    id: 3,
-    title: 'Very complete class',
+    id: "Ggt1YgcObl4",
+    title: 'Patient from Uganda | Brain Aneurysm, Neurosurgery',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     user: {
@@ -38,8 +38,8 @@ export const data: Array<Testimonial> = [
     },
   },
   {
-    id: 4,
-    title: 'Great Quality!',
+    id: "ucV40GA6FOM",
+    title: 'Patient from Kenya | Cerebral Palsy treatment in India',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     user: {
@@ -47,18 +47,6 @@ export const data: Array<Testimonial> = [
       name: 'Diana Jordan',
       professional: 'SEO Expert',
       photo: '4.jpg',
-    },
-  },
-  {
-    id: 5,
-    title: 'Detailed learning materials',
-    content:
-      'Classes that provide very detailed material in term of making UI UX Design starting team making low and hight quality, system designs, using data layout and make prototypes and testing.',
-    user: {
-      id: 1,
-      name: 'Ashley Graham',
-      professional: 'Back-End Developer',
-      photo: '5.jpg',
     },
   },
 ]
